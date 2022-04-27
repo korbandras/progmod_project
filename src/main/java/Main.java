bfab9213b868004f87dd6d99d18955798048bdbf
korbandras <korbandras@gmail.com>
2022-04-27 19:00:00 +0200

    Átlag számoló megadott jegyekből+

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import static java.lang.System.err;
 import static java.lang.System.out;
 
 
@@ -6,22 +9,34 @@ public class Main {
     public static void main(String[] args) {
         Methods meth = new Methods();
         Scanner be = new Scanner(System.in);
-        out.println("1 - Average\n2 - Delete grade\n3 - Modify grade\n4 - Calculate\n5 - Exit");
-        out.println("Choose from the options above:");
-        int valaszt = be.nextInt();
-        while(valaszt != 5 || valaszt >= 6 || valaszt <= 0){
-            if(valaszt == 1){
-                out.printf("The average of the given grades: %.2f", meth.avg());
+        try {
+            out.println("1 - Average\n2 - KKI\n3 - Modify grade\n4 - Calculate\n5 - Exit");
+            out.println("Choose from the options above:");
+            int valaszt = be.nextInt();
+            while (valaszt != 5 || valaszt >= 6 || valaszt <= 0) {
+                if (valaszt == 1) {
+                    out.printf("The average of the given grades: %.2f\n", meth.avg());
+                    out.println("Choose from the previous options above:");
+                    valaszt = be.nextInt();
+                }
+                if (valaszt == 2) {
+                    meth.kki();
+                    out.println("Choose from the previous options above:");
+                    valaszt = be.nextInt();
+                }
+                if (valaszt == 3) {
+                    out.println("3");
+                }
+                if (valaszt == 4) {
+                    out.println("4");
+                }
             }
-            if(valaszt == 2){
-                out.println("2");
-            }
-            if(valaszt == 3){
-                out.println("3");
-            }
-            if(valaszt == 4) {
-                out.println("4");
-            }
+        }
+        catch(InputMismatchException e){
+            err.println("Not a number.");
+        }
+        finally{
+            out.println("End of program - Exiting");
         }
     }
 }
--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,29 +1,52 @@
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import static java.lang.System.out;
 
 public class Methods {
+
+    private ArrayList<Object> list0 = new ArrayList();
+
     public double avg(){
         Scanner be = new Scanner(System.in);
         double average;
         int grade;
         double sum = 0, howmany;
-        ArrayList<Integer> list = new ArrayList<>();
+        String sub;
+        ArrayList<String> list = new ArrayList<>();
+        //DocumentBuilderFactory build = DocumentBuilderFactory.newInstance();
         out.println("How many grades you have?");
         howmany = be.nextInt();
         for(int i = 0; i < howmany; i++){
             out.printf("Insert your %d. grade: ", i+1);
             grade = be.nextInt();
-            list.add(grade);
-            sum = sum + grade;
+            if(grade >= 1 && grade <= 5){
+                String thingy = String.valueOf(grade);
+                //String thingy = grade + sub + crd;
+                list.add(thingy);
+                sum = sum + grade;
+            }
+            else{
+                out.println("Given number not a grade.");
+                out.printf("Insert your %d. grade: ", i+1);
+                grade = be.nextInt();
+                String thingy = String.valueOf(grade);
+                list.add(thingy);
+                sum = sum + grade;
+            }
+            /*
+            out.printf("%d. subject:", i+1);
+            sub = be.nextLine();
+            out.println();
+            out.print("Credit:");
+            int crd = be.nextInt();
+            */
         }
+        list0.add(list);
         average = sum / howmany;
-        /*
-        out.printf("Átlag: %.2f", average);
-        out.println();
-        out.println(list);
-         */
         return average;
+    }
+    public void kki(){
+        out.println(list0);
     }
 }
